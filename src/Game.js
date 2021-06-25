2c59bd50ba29b2f10af25acd55408d279d25aaed
Matt Lane <lane.matthew@gmail.com>
2021-06-25 10:41:44 -0700

    add basic Game component

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,32 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Game() {
-  return <h1>Game</h1>;
+  const [gameData, setGameData] = useState(null);
+  const { gameId } = useParams();
+
+  useEffect(() => {
+    const getGameData = async () => {
+      const result = await fetch(`http://localhost:3001/categories/${gameId}`);
+      const data = await result.json();
+      setGameData(data.category);
+    };
+
+    getGameData();
+  }, [gameId]);
+
+  if (!gameData) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>{gameData.title}</h1>
+      <ul>
+        {gameData.answers.map(answer => (
+          <li key={answer.id}>{answer.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Game;
